0f86de44f6a555f74b25e91e94d06171fea8dfb6
zferguson24 <71412122+zferguson24@users.noreply.github.com>
2021-12-10 20:21:59 -0500

    Added Update/Delete to User/eBook
    All CRUD operations are programmed. Don't wait until the day-of to commit and push stuff. At the very least, put what you've done so far in the group discord chat, otherwise it looks like you've done nothing. Going to add some admin buttons to make navigation easier.

--- index.js
+++ index.js
@@ -38,13 +38,15 @@ app.get("/", (req, res) => {
 
 // Add eBook Logic
 app.get("/addebook", (req, res) => {
-    res.render("addeBook");
+  res.render("addeBook");
 });
 
 app.post("/insertebook", (req, res) => {
-  let data = {EBOOK_TITLE: req.body.eBookTitle, EBOOK_ISBN: req.body.eBookISBN, EBOOK_AUTHOR: req.body.eBookAuthor,
-    EBOOK_GENRE: req.body.eBookGenre, EBOOK_PRICE: req.body.eBookPrice, EBOOK_DESC: req.body.eBookDesc, 
-    EBOOK_PUB: req.body.eBookPub};
+  let data = {
+    EBOOK_TITLE: req.body.eBookTitle, EBOOK_ISBN: req.body.eBookISBN, EBOOK_AUTHOR: req.body.eBookAuthor,
+    EBOOK_GENRE: req.body.eBookGenre, EBOOK_PRICE: req.body.eBookPrice, EBOOK_DESC: req.body.eBookDesc,
+    EBOOK_PUB: req.body.eBookPub
+  };
   let sql = `INSERT INTO EBOOK SET ?`;
   let query = db.query(sql, data, (err, result) => {
     if (err) {
@@ -103,8 +105,90 @@ app.post("/searchebook", (req, res) => {
   });
 });
 
+// Admin Search eBook Logic
+app.get("/ebooksearch", (req, res) => {
+  res.render("eBookSearch");
+});
 
-// Search User Logic
+app.post("/adminsearchebook", (req, res) => {
+  let sql = `SELECT * FROM EBOOK WHERE EBOOK_TITLE LIKE '%${req.body.admineBookSearch}%';`;
+  let query = db.query(sql, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.render("returneBook", { data: result });
+  });
+});
+
+// Admin Update eBook Logic
+app.post("/ebookupdate", (req, res) => {
+  if (req.body.ebookID == undefined) {
+    res.send(`No eBook to update. Please search again.`);
+  } else {
+    let sql = `UPDATE EBOOK SET `;
+
+    // Update EBOOK_TITLE if nonempty.
+    if (req.body.ebookTitle != undefined) {
+      sql = sql + `EBOOK_TITLE = '${req.body.ebookTitle}', `;
+    }
+    // Update EBOOK_ISBN if nonempty.
+    if (req.body.ebookISBN != undefined) {
+      sql = sql + `EBOOK_ISBN = '${req.body.ebookISBN}', `;
+    }
+    // Update EBOOK_AUTHOR if nonempty.
+    if (req.body.ebookAuthor != undefined) {
+      sql = sql + `EBOOK_AUTHOR = '${req.body.ebookAuthor}', `;
+    }
+    // Update EBOOK_GENRE if nonempty.
+    if (req.body.ebookGenre != undefined) {
+      sql = sql + `EBOOK_GENRE = '${req.body.ebookGenre}', `;
+    }
+    // Update EBOOK_PRICE if nonempty.
+    if (req.body.ebookPrice != undefined) {
+      sql = sql + `EBOOK_PRICE = '${req.body.ebookPrice}', `;
+    }
+    // Update EBOOK_DESC if nonempty.
+    if (req.body.ebookDesc != undefined) {
+      sql = sql + `EBOOK_DESC = '${req.body.ebookDesc}', `;
+    }
+    // Update EBOOK_PUB if nonempty.
+    if (req.body.ebookPub != undefined) {
+      sql = sql + `EBOOK_PUB = '${req.body.ebookPub}' `;
+    }
+
+    // Deal with potentially having an extra, incorrect comma.
+    if (sql.slice(-1) == `,`) {
+      sql = sql.substring(0, sql.length - 1);
+    }
+
+    // Add WHERE clause to UPDATE to pick the correct eBook.
+    sql = sql + ` WHERE EBOOK_ID = '${req.body.ebookID}';`;
+
+    let query = db.query(sql, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.send(`eBook has been updated successfully.`);
+    });
+  }
+});
+
+// Admin Delete eBook Logic
+app.post("/ebookdelete", (req, res) => {
+  if (req.body.ebookID == undefined) {
+    res.send(`No eBook to delete. Please search again.`);
+  } else {
+    let sql = `DELETE FROM EBOOK WHERE EBOOK_ID = '${req.body.ebookID}';`
+    let query = db.query(sql, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.send(`eBook has been deleted from the database successfully.`);
+    });
+  }
+});
+
+// Admin Search User Logic
 app.get("/usersearch", (req, res) => {
   res.render("userSearch");
 });
@@ -119,41 +203,60 @@ app.post("/searchuser", (req, res) => {
   });
 });
 
-// Update User Logic
+// Admin Update User Logic
 app.post("/updateuser", (req, res) => {
-  let sql = `UPDATE USER SET `;
+  if (req.body.userID == undefined) {
+    res.send(`No user to update. Please search again.`);
+  } else {
+    let sql = `UPDATE USER SET `;
 
-  // Update USER_EMAIL if nonempty.
-  if (req.body.userEmail != undefined) {
-    sql = sql + `USER_EMAIL = '${req.body.userEmail}', `;
-  }
-  // Update USER_FNAME if nonempty.
-  if (req.body.userFname != undefined) {
-    sql = sql + `USER_FNAME = '${req.body.userFname}', `;
-  }
-  // Update USER_LNAME if nonempty.
-  if (req.body.userLname != undefined) {
-    sql = sql + `USER_LNAME = '${req.body.userLname}', `;
-  }
-  // Update USER_DOB if nonempty.
-  if (req.body.userDOB != undefined) {
-    sql = sql + `USER_DOB = '${req.body.userDOB}' `;
-  }
-
-  // Deal with potentially having an extra, incorrect comma.
-  if (sql.slice(-1) == `,`) {
-    sql = sql.substring(0, sql.length - 1);
-  }
-
-  // Add WHERE clause to UPDATE to pick the correct user.
-  sql = sql + ` WHERE USER_ID = '${req.body.userID}';`;
-
-  let query = db.query(sql, (err, result) => {
-    if (err) {
-      throw err;
+    // Update USER_EMAIL if nonempty.
+    if (req.body.userEmail != undefined) {
+      sql = sql + `USER_EMAIL = '${req.body.userEmail}', `;
     }
-    res.send(`User has been updated successfully.`);
-  });
+    // Update USER_FNAME if nonempty.
+    if (req.body.userFname != undefined) {
+      sql = sql + `USER_FNAME = '${req.body.userFname}', `;
+    }
+    // Update USER_LNAME if nonempty.
+    if (req.body.userLname != undefined) {
+      sql = sql + `USER_LNAME = '${req.body.userLname}', `;
+    }
+    // Update USER_DOB if nonempty.
+    if (req.body.userDOB != undefined) {
+      sql = sql + `USER_DOB = '${req.body.userDOB}' `;
+    }
+
+    // Deal with potentially having an extra, incorrect comma.
+    if (sql.slice(-1) == `,`) {
+      sql = sql.substring(0, sql.length - 1);
+    }
+
+    // Add WHERE clause to UPDATE to pick the correct user.
+    sql = sql + ` WHERE USER_ID = '${req.body.userID}';`;
+
+    let query = db.query(sql, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.send(`User has been updated successfully.`);
+    });
+  }
+});
+
+// Admin Delete User Logic
+app.post("/deleteuser", (req, res) => {
+  if (req.body.userID == undefined) {
+    res.send(`No user to delete. Please search again.`);
+  } else {
+    let sql = `DELETE FROM USER WHERE USER_ID = '${req.body.userID}';`
+    let query = db.query(sql, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.send(`User has been deleted from the database successfully.`);
+    });
+  }
 });
 
 // Shows entire EBOOK table.
